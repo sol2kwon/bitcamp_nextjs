5c1d9679306df83e06d2e157c6e227f0b8b6a3bc
sol2kwon <amorfatisole@gmail.com>
2022-03-27 11:57:52 +0900

    2022-03-27 bmi

--- src/pages/bmi.js
+++ src/pages/bmi.js
@@ -6,28 +6,30 @@ const [inputs,setInputs] = useState({})
 const [result,setResult] = useState('')
 const {name,weight,height} = inputs;
 
+
 const handleChange =(e)=>{
-    e.preventDerault()
+    e.preventDefault()
     const{value,name} = e.target;
     setInputs({...inputs,[name]:value})
 }
 const hadleClick = (e)=>{
-    e.preventDerault()
+    e.preventDefault()
     setResult(`${name}님의 BMI 결과는 ${(weight *10000/height/height).toFixed(2)}입니다.`)
 
 }
+
 return <div>
 <h1>BMI</h1>
 
 <form>
     <label><b>이름</b></label>
-    <inputs type ="text" onChange = {handleChange} name = "name"/><br/>
+    <input type ="text" onChange = {handleChange} name = "name"/><br/>
 
     <label><b>몸무게</b></label>
-    <inputs type ="text" onChange = {handleChange} name = "weight"/><br/>
+    <input type ="text" onChange = {handleChange} name = "weight"/><br/>
 
     <label><b>키</b></label>
-    <inputs type ="text" onChange = {handleChange} name = "height"/><br/>
+    <input type ="text" onChange = {handleChange} name = "height"/><br/>
 
     <button onClick = {hadleClick}>계산하기</button>
 </form>
