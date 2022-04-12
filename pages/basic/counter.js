89530f292b716a569574b79e829869349ed5025a
sol2kwon <amorfatisole@gmail.com>
2022-04-12 10:35:44 +0900

    first commit

--- pages/basic/counter.js
+++ pages/basic/counter.js
@@ -1,10 +1,13 @@
 import React,{useState} from "react"
+ 
 export default function Counter(){
     const [count, setCount] = useState(0)
 
-    return(<div>
+    return(
+        <div>
         <button onClick={()=>{setCount(count + 1)}}>+</button> <br/>
         <button onClick={()=>(setCount(count -1))}>-</button> <br/>
         <div>{count}</div>
         </div>)
+        
 }
